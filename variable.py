f2afd2e7d9a840760e1c3b05bfba89a492f82761
abiryusuf <yusuf_ctg@yahoo.com>
2021-02-26 21:28:27 -0500

    variable

--- variable.py
+++ variable.py
@@ -6,3 +6,25 @@ print(x)
 #  get the type
 y = 5
 print(type(y))
+
+# assign multiple values
+Abir = Yusuf = Mim = "Banglades"
+print(Abir, Yusuf, Mim)
+
+color = ["A", "B", "c"]
+print(color)
+
+x = 5
+y = 7
+print(x + y)
+
+#global variable
+
+name = "Proramming language"
+def function(x):
+    #write a global inside the function it will be global
+    global r 
+    r = 10
+    print("Python is " + name, x * 2)
+function(2)
+
