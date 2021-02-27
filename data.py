8531e780255a15a32db4213c5c7a25ac1a564052
abiryusuf <yusuf_ctg@yahoo.com>
2021-02-26 21:57:55 -0500

    data

---
+++ data.py
@@ -0,0 +1,18 @@
+# Text Type:	str
+name = "abir"
+print(type(name))
+# Numeric Types:	int, float, complex
+x = 5
+y = 5.7
+z = 1j
+# Sequence Types:	list, tuple, range
+x = ["A", "B", "C"]
+x = ("A", "B", "C")
+x = range(6)
+# Mapping Type:	dict
+x = {"name" : "John", "age" : 36}
+# Set Types:	set, frozenset
+x = {"A", "B", "C"}
+# Boolean Type:	bool
+x = True
+# Binary Types:	bytes, bytearray, memoryview
