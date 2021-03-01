72cd3bfa8eb7fe1e5360fcfece77f2a316a5177f
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-01 12:57:06 -0500

    list

---
+++ lists/List.py
@@ -0,0 +1,15 @@
+
+# List is collection of data or items in single variable
+
+thisList = ["abir", "yusuf", "mim", "madiha"]
+
+print(thisList)
+
+print(len(thisList))
+
+for a in thisList:
+    print(a)
+
+listOne = ["abc", 34, True, 40, "male"]
+print(listOne)
+print(type(listOne))
