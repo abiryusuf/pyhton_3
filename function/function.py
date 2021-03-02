4bfdf48adf0672ac266c3d6b46188cccbd12b370
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-01 20:15:51 -0500

    function

---
+++ function/function.py
@@ -0,0 +1,13 @@
+
+# Function is block of codes and it will execute when it is called
+
+# def is keyword for python function
+def myFunction():
+    print("Hello Funtion ")
+
+myFunction()
+
+def argFunction(fName, lName):
+    print(fName + " " + lName)
+
+argFunction("Abir", "Yusuf")
