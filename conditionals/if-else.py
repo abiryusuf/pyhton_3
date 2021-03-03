0eba4a0581c7600e840fcdf805b39f4a7f7c2ad0
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-03 18:08:07 -0500

    if else

---
+++ conditionals/if-else.py
@@ -0,0 +1,8 @@
+print("cat" + "Cat")
+
+def userName(username):
+    if len(username) < 3:
+        print("Invalied username")
+    else:
+        print("Valid User Name")
+userName("ab")            
