2e871be827f5ca7a811947c707f8244b001d758a
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-05 15:44:22 -0500

    algo

--- demo.py
+++ demo.py
@@ -1,8 +1,18 @@
-x = 100
+# x = 100
 
-y = 50
+# y = 50
 
 # if(x ** 2 > 100 and y < 40):
-print(x and y)
+# print(x and y)
 
-print(2** 3 ** 2)
+# print(2** 3 ** 2)
+
+# range
+for x in range(4,10):
+    print(x)
+
+
+def to_celsius(x):
+    return (x - 32)*5/9
+for y in range(0,101, 10):
+    print(y, to_celsius(y))
--- loop/forLoop.py
+++ loop/forLoop.py
@@ -64,3 +64,19 @@ for value in values:
     sum1 += value
     length += 1
 print("Total sum: " + str(sum1) + " - Average: " + str(sum1/length))
+
+
+def factorial(n):
+    result = 1
+    for i in range(1, n+1):
+        result = result * i
+    return result
+
+print(factorial(4)) # should return 24
+print(factorial(5)) # should return 120
+
+# To quickly recap the range() function when passing one, two, or three parameters:
+
+# One parameter will create a sequence, one-by-one, from zero to one less than the parameter.
+# Two parameters will create a sequence, one-by-one, from the first parameter to one less than the second parameter.
+# Three parameters will create a sequence starting with the first parameter and stopping before the second parameter, but this time increasing each step by the third parameter.
