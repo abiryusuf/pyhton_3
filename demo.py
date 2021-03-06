81e6f920464c6c6fae7ed03a845a5d9a08c85a66
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-05 20:59:33 -0500

    python algorithm

--- demo.py
+++ demo.py
@@ -8,11 +8,31 @@
 # print(2** 3 ** 2)
 
 # range
-for x in range(4,10):
-    print(x)
+# for x in range(4,10):
+#     print(x)
 
 
-def to_celsius(x):
-    return (x - 32)*5/9
-for y in range(0,101, 10):
-    print(y, to_celsius(y))
+# def to_celsius(x):
+#     return (x - 32)*5/9
+# for y in range(0,101, 10):
+#     print(y, to_celsius(y))
+
+
+# teams = [ 'Dragons', 'Wolves', 'Pandas', 'Unicorns']
+# for home_team in teams:
+#     for away_team in teams:
+#         if home_team != away_team:
+            #print(home_team + " vs " + away_team)
+
+
+# for s in ([25]):
+#     print(s)
+
+# for x in range (1, 10, 3):
+#     print(x)
+
+for x in range(10):
+    for y in range(x):
+        print(y)
+
+      
