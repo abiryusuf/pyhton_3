fc933c5274c0e4a2513ea0451e00e6631898b3b4
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-07 22:28:29 -0500

    string algo

---
+++ stringAlgo.py
@@ -0,0 +1,33 @@
+def is_palindrome(input_string):
+	# We'll create two strings, to compare them
+	new_string = ""
+	reverse_string = ""
+	# Traverse through each letter of the input string
+	for x in input_string:
+		# Add any non-blank letters to the 
+		# end of one string, and to the front
+		# of the other string. 
+		if x != "":
+			new_string = new_string + x
+			reverse_string = x + reverse_string
+	# Compare the strings
+	if new_string == reverse_string:
+		return True
+	return False
+
+print(is_palindrome("Never Odd or Even")) # Should be True
+print(is_palindrome("abc")) # Should be False
+print(is_palindrome("kayak")) # Should be True
+
+
+def convert_distance(miles):
+	km = miles * 1.6 
+	result = "{} miles equals {:.1f} km".format(miles, km)
+	return result
+
+print(convert_distance(12)) # Should be: 12 miles equals 19.2 km
+print(convert_distance(5.5)) # Should be: 5.5 miles equals 8.8 km
+print(convert_distance(11)) # Should be: 11 miles equals 17.6 km
+
+x = "Rainfall"
+print(x[:4])
