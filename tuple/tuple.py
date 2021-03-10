5bbe39b258c07baa1c500cccb8940294de1d36b4
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-10 14:33:21 -0500

    tuple in list

--- tuple/tuple.py
+++ tuple/tuple.py
@@ -35,3 +35,11 @@ def skip_elements(elements):
 
 print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
 print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) # Should be ['Orange', 'Strawberry', 'Peach']
+
+
+def fullName(people):
+	result = []
+	for email, name in people:
+		result.append("{} <{}>".format(name, email))
+	return result
+print(fullName([("abir@gmail.com", "abir")]))
