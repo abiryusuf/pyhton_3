7e714277379b762861cfeff680a57b02951edf43
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-11 21:46:31 -0500

    dictionries

--- lists/dictionaries/quiz.py
+++ lists/dictionaries/quiz.py
@@ -1,7 +1,12 @@
 def email_list(domains):
     email =[]
-    for users, domain in domains.items():
+    for domain, users in domains.items():
         for user in users:
             email.append(user + "@" + domain)
-        return email
+    return email
 print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
+
+
+wardrobe = {'shirt': ['red', 'blue', 'white'], 'jeans': ['blue', 'black']}
+new_items = {'jeans': ['white'], 'scarf': ['yellow'], 'socks': ['black', 'brown']}
+wardrobe.update(new_items)
