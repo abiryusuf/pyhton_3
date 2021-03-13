aab09ccbd55bf968497d1704dca2a40796bfb8cd
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-13 12:17:43 -0500

    formate

--- string/Format.py
+++ string/Format.py
@@ -2,19 +2,19 @@
 # We can not combaine string and int
 # by using formate method we can combine str and int
 
-# age = 32
-# place = "nY"
+age = 32
+place = "nY"
 
-# name = " I am Abir, and My age is {} and Living {}"
+name = " I am Abir, and My age is {} and Living {}"
 
-# print(name.format(age, place))
+print(name.format(age, place))
 
-# q = 3
-# item = 653
-# price = 50.89
+q = 3
+item = 653
+price = 50.89
 
-# myOrder = "I want to pay {2} dollars for {0} pieces of item {1}"
-# print(myOrder.format(q, item, price))
+myOrder = "I want to pay {2} dollars for {0} pieces of item {1}"
+print(myOrder.format(q, item, price))
 
 name = "Abir"
 number = len(name) * 3
