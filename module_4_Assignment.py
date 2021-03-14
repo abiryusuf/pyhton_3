fd821b93d08543e8156956846b538a5c2060b32d
abiryusuf <yusuf_ctg@yahoo.com>
2021-03-14 13:51:59 -0400

    module 4

--- module_4_Assignment.py
+++ module_4_Assignment.py
@@ -33,3 +33,63 @@ print(format_address("1001 1st Ave"))
 
 print(format_address("55 North Center Drive"))
 # Should print "house number 55 on street named North Center Drive"
+
+# 2
+# The highlight_word function changes the given word in a sentence to its upper-case version. 
+# For example, highlight_word("Have a nice day", "nice") returns "Have a NICE day". Can you write this function in just one line?
+def highlight_word(sentence, word):
+	return(sentence.replace(word, word.upper()))
+
+print(highlight_word("Have a nice day", "nice"))
+print(highlight_word("Shhh, don't be so loud!", "loud"))
+print(highlight_word("Automating with Python is fun", "fun"))
+
+
+# 3
+# A professor with two assistants, Jamie and Drew, wants an attendance list of the students, in the order that they arrived in the classroom.
+#  Drew was the first one to note which students arrived, and then Jamie took over. 
+# After the class, they each entered their lists into the computer and emailed them to the professor, 
+# who needs to combine them into one, in the order of each student's arrival. Jamie emailed a follow-up, saying that her list is in reverse order. Complete the steps to combine them into one list as follows: the contents of Drew's list, followed by Jamie's list in reverse order, to get an accurate list of the students as they arrived.
+
+def combine_lists(list1, list2):
+  # Generate a new list containing the elements of list2
+  # Followed by the elements of list1 in reverse order
+  new_list = list2
+  for name in reversed(range(len(list1))):
+    new_list.append(list1[name])
+  return new_list
+  
+	
+Jamies_list = ["Alice", "Cindy", "Bobby", "Jan", "Peter"]
+Drews_list = ["Mike", "Carol", "Greg", "Marcia"]
+
+print(combine_lists(Jamies_list, Drews_list))
+
+
+
+
+# Question 7
+# Use a dictionary to count the frequency of letters in the input string. Only letters should be counted, not blank spaces, numbers, or punctuation.
+#  Upper case should be considered the same as lower case. For example, count_letters("This is a sentence.") 
+# should return {'t': 2, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 3, 'n': 2, 'c': 1}.
+
+def count_letters(text):
+  result = {}
+  # Go through each letter in the text
+  for letter in text:
+    # Check if the letter needs to be counted or not
+    if letter not in result:
+      result[letter.lower()] = 1
+    # Add or increment the value in the dictionary
+    else:
+      result[letter.lower()] += 1
+  return result
+
+print(count_letters("AaBbCc"))
+# Should be {'a': 2, 'b': 2, 'c': 2}
+
+print(count_letters("Math is fun! 2+2=4"))
+# Should be {'m': 1, 'a': 1, 't': 1, 'h': 1, 'i': 1, 's': 1, 'f': 1, 'u': 1, 'n': 1}
+
+print(count_letters("This is a sentence."))
+# Should be {'t': 2, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 3, 'n': 2, 'c': 1}
